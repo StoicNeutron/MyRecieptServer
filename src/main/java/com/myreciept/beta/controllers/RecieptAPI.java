6295ec395262397fff1e8446af668f1a73f10ce1
StoicNeutron <stoicneutron@Samnangs-MacBook-Pro.local>
2024-02-16 16:44:22 -0500

    add initial file

---
+++ src/main/java/com/myreciept/beta/controllers/RecieptAPI.java
@@ -0,0 +1,12 @@
+package com.myreciept.beta.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Samnang Thorn
+ */
+@RestController
+public class RecieptAPI {
+
+    // Endpoints API are here
+}
